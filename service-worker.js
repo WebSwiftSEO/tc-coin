460fb262fd094603449e0068ef400ff5b2a24315
Ylia Callan 🎼🕸️🪞 <yliacallan@gmail.com>
2025-03-16 18:12:30 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,6 @@ const cacheName = 'tc-coin-v1';
 const assets = [
     '/Tc-Coin-Blockchain/',
     '/Tc-Coin-Blockchain/index.html',
-    '/Tc-Coin-Blockchain/styles.css',
-    '/Tc-Coin-Blockchain/app.js',
-    '/Tc-Coin-Blockchain/manifest.json',
     '/Tc-Coin-Blockchain/images/coin-tc-blockchain-512x512.webp',
     '/Tc-Coin-Blockchain/images/qr-bg-tc-blockchain-300x300.webp'
 ];
